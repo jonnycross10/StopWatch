a477d8e0912658a3a7b77ee2c3262fa51527f1a9
Jonny Cross <crossjonny5@gmail.com>
2020-07-01 15:02:37 -0700

    Add project files.

---
+++ SW/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(227)))), ((int)(((byte)(179)))));
+            this.label1.Location = new System.Drawing.Point(200, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00.0000000";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(210)))), ((int)(((byte)(220)))));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(227)))), ((int)(((byte)(179)))));
+            this.button1.Location = new System.Drawing.Point(209, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(210)))), ((int)(((byte)(220)))));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(227)))), ((int)(((byte)(179)))));
+            this.button2.Location = new System.Drawing.Point(446, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(100)))), ((int)(((byte)(142)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SW/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SW
+{
+    public partial class Form1 : Form
+    {
+        //trackers
+        bool paused = false;
+        bool reset = true;
+        
+        //initialize start time
+        DateTime startTime = DateTime.MinValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (!paused && reset)
+            {
+                //start over
+                startTime = DateTime.Now;
+                paused = false;
+                reset = false;
+            }
+            else if (!paused && !reset)
+            {
+                //pause
+                TimeSpan newb = DateTime.Now - startTime;
+                startTime = DateTime.MinValue + newb;
+                //MessageBox.Show("counted seconds: " + (DateTime.Now - startTime).ToString());
+                paused = true;
+            }
+            else if(paused)
+            {
+                //resume
+                TimeSpan newb = DateTime.Now - startTime;
+                startTime = DateTime.MinValue + newb;
+                paused = false;
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //reset
+            startTime = DateTime.MinValue;
+            label1.Text = "00:00:00.0000000";
+            paused = false;
+            reset = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //update
+            if (!paused)
+            {
+                label1.Text = startTime == DateTime.MinValue ? "00:00:00.0000000" : (DateTime.Now - startTime).ToString();
+            }
+            
+        }
+
+        
+    }
+}
